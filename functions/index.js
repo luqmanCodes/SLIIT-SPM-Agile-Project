812bf11e53a9b242b6a5299ce0b6602810096aa9
luqmanCodes <37939871+luqmanCodes@users.noreply.github.com>
2018-09-09 01:53:50 +0530

    added more design to formI1

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-
+const nodemailer = require('nodemailer');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,3 +7,6 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
+exports.emailFormI = functions.https.onRequest((req,res) => {
+    res.send('hello');
+})
