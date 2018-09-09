88b17cce9be4acac71d6f72c2f645ad81b2611de
ghaziIsmail <gazzsmile@gmail.com>
2018-09-09 07:01:19 +0530

    company registration

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React, { Component } from 'react';
 import './App.css';
+//import firebase from 'firebase';
+//import {DB_CONFIG} from './config';
+import CompanyRegistration from './containers/CompanyRegistration';
+import Test from './containers/Test';
 
 class App extends Component {
+ /* constructor(props){
+    super(props);
+    this.app=firebase.initializeApp(DB_CONFIG);
+  }*/
   render() {
     return (
       <div className="App">
-        
+        <div><CompanyRegistration/></div>
       </div>
     );
   }
