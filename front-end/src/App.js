0d612634d64d95cd841da658b05c9fce811a3686
mohammedsuhail85 <suhailmsm@gmail.com>
2018-09-09 07:39:46 +0530

    added react router and added a dummy ui

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import './App.css';
-
 import FormI5 from './components/Form_I-5/FormI5';
+import Menu from './components/MainUI/Menu';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <FormI5 />
-      </div>
-    );
+      <Router>
+        <div>
+          <Route exact path='/' component={Menu}/>
+          <Route exact path='/student' component={Menu}/>
+          <Route exact path='/supervisor' component={Menu}/>
+          <Route exact path='/formI5' component={FormI5}/>
+        </div>
+      </Router>
+);
   }
 }
 
