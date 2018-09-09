c922cc3f998a055c99299dd3b00e0b232e8ca082
Luqman Mohammed <37939871+luqmanCodes@users.noreply.github.com>
2018-09-09 09:18:24 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-const nodemailer = require('nodemailer');
+//const nodemailer = require('nodemailer');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
