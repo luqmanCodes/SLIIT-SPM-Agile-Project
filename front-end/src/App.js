302bb25ef3bf4a2d17b7a64aadc4595581d5b500
luqmanCodes <37939871+luqmanCodes@users.noreply.github.com>
2018-09-18 19:37:55 +0530

    merge conflict

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {Route} from 'react-router-dom';
-import './App.css';
-
-import FormI5 from './components/Form_I-5/FormI5';
-import Menu from './components/MainUI/Menu';
-
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import firebase from 'firebase';
 import {DB_CONFIG} from './config';
-import FormI1 from './containers/form.I-1.student';
-class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.app = firebase.initializeApp(DB_CONFIG);
-  } 
+const fireApp = firebase.initializeApp(DB_CONFIG);
+export const app = fireApp; 
+class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Route exact path='/' component={Menu }/>
-          <Route exact path='/student' component={FormI1}/>
-          <Route exact path='/supervisor' component={Menu}/>
-          <Route exact path='/formI5' component={FormI5}/>
-        </div>
+        <Switch>
+          <Route exact path='/' component={Login}/>
+        </Switch>
       </Router>
     );
   }
