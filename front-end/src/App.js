b6993ad71042b414a99a980bd94e24be4e482c46
Luqman Mohammed <37939871+luqmanCodes@users.noreply.github.com>
2018-10-06 18:42:36 +0530

    Update App.js
    removed bootstrap dependencies

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import $ from 'jquery';
-// import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React, { Component } from 'react';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import Login from './components/Auth/LoginForm';
